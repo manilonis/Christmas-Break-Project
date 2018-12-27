2b74b1aa147b4db3438efa2041d85cb3a03a68ba
manilonis <manilonis741@gmail.com>
2018-12-27 17:29:08 -0500

    Parent Issue and Memory Management

--- Mike-Dev/Issue.hpp
+++ Mike-Dev/Issue.hpp
@@ -6,10 +6,13 @@ class Issue{
  	int id_num;
  	string name;
  	string description;
+ 	Issue* parent;
 
  public:
  	Issue(int, string, string);
+ 	Issue(int, string, string, Issue*);
  	int get_id_num();
  	string get_name();
  	string get_desript();
+ 	Issue* get_parent();
 };
--- Mike-Dev/Test_Issue.cpp
+++ Mike-Dev/Test_Issue.cpp
@@ -6,6 +6,11 @@ using namespace std;
 
 int main(){
 	Issue *i = new Issue(1, "First", "The First");
+	Issue *i2 = new Issue(2, "Child", "Child", i);
 	cout << i->get_id_num() << "\n" ;
+	cout << i2->get_parent()->get_name() << "\n";
+
+	delete(i);
+	delete(i2);
 
 }
